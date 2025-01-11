92a62599e3a2bedef8412bc187523c93b9952923
Azem <azemchap@gmail.com>
2025-01-11 03:38:07 +0100

    linting errors

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,24 +1,33 @@
 import PostPage from '@/components/sections/PostPage';
 import { prisma } from '@/lib/db';
+// import { Post } from '@prisma/client'; // Import Post type for better type safety
 
 interface Params {
     slug: string;
 }
 
-export default async function Page({ params }: { params: Params }) {
+// interface PageProps {
+//     post?: Post; // Optional post prop
+//     error?: string; // Optional error prop
+// }
 
+export default async function Page({ params }: { params: Params }): Promise<JSX.Element> {
     try {
         const post = await prisma.post.findUnique({
-            where: { slug: params.slug }
+            where: { slug: params.slug },
         });
 
         if (!post) {
-            return <p>Post not found </p>;
+            return <p>Post not found</p>; // Handle case where the post does not exist
         }
 
-        return <PostPage post={post} />;
+        return <PostPage post={post} />; // Render the PostPage component
     } catch (error) {
         console.error("Error fetching post:", error);
-        return { props: { error: "Failed to fetch post" } };
+        return (
+            <div>
+                <p>Error: Failed to fetch post</p>
+            </div>
+        ); // Handle error gracefully
     }
 }
