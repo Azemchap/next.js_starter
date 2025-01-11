b457e292c1c484eae2bc4a31c5882012b7e3d71d
Azem <azemchap@gmail.com>
2025-01-11 10:54:10 +0100

    SinglePostPage

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -7,34 +7,20 @@ interface Params {
     slug: string;
 }
 
-export default async function Page({ params }: { params: Params }) {
+export default async function SinglePostPage({ params }: { params: Params }) {
     // try {
-    //     const post = await prisma.post.findUnique({
-    //         where: { slug: params.slug },
-    //     });
+    console.log("Fetching post with slug:", params.slug);
+    const post = await prisma.post.findUnique({
+        where: { slug: params.slug },
+    });
 
-    //     if (!post) {
-    //         return <p>Post not found</p>; // Return a fallback message
-    //     }
+    if (!post) {
+        return <p>Post not found</p>; // Return a fallback message
+    }
 
-    //     return <PostPage post={post} />;
+    return <PostPage post={post} />;
     // } catch (error) {
     //     console.error("Error fetching post:", error);
     //     return <p>Error: Failed to fetch post</p>; // Gracefully handle the error
     // }
-    try {
-        console.log("Fetching post with slug:", params.slug);
-        const post = await prisma.post.findUnique({
-            where: { slug: params.slug },
-        });
-
-        if (!post) {
-            return <p>Post not found</p>; // Return a fallback message
-        }
-
-        return <PostPage post={post} />;
-    } catch (error) {
-        console.error("Error fetching post:", error);
-        return <p>Error: Failed to fetch post</p>; // Gracefully handle the error
-    }
 }
