7446e4c227378495e1bac752f9a6bc451a35a4da
Azem <azemchap@gmail.com>
2025-01-11 04:04:37 +0100

    another one

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -8,7 +8,22 @@ interface Params {
 }
 
 export default async function Page({ params }: { params: Params }) {
+    // try {
+    //     const post = await prisma.post.findUnique({
+    //         where: { slug: params.slug },
+    //     });
+
+    //     if (!post) {
+    //         return <p>Post not found</p>; // Return a fallback message
+    //     }
+
+    //     return <PostPage post={post} />;
+    // } catch (error) {
+    //     console.error("Error fetching post:", error);
+    //     return <p>Error: Failed to fetch post</p>; // Gracefully handle the error
+    // }
     try {
+        console.log("Fetching post with slug:", params.slug);
         const post = await prisma.post.findUnique({
             where: { slug: params.slug },
         });
