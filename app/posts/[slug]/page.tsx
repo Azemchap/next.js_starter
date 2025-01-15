9312db01f331c7a4e47978a5bb62df6458bc67d2
Azem <38106778+Azemchap@users.noreply.github.com>
2025-01-15 09:36:25 +0100

    update slug page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,26 @@
-/* eslint-disable */
+
+
+// import PostPage from '@/components/sections/PostPage';
+// import NotFound from '@/components/shared/NotFound';
+// import prisma from '@/lib/db';
+
+// interface Params {
+//     slug: string;
+// }
+
+// export default async function SinglePostPage({ params }: { params: Params }) {
+
+//     console.log("Fetching post with slug:", params.slug);
+//     const post = await prisma.post.findUnique({
+//         where: { slug: params.slug },
+//     });
+
+//     if (!post) {
+//         return <NotFound message={'Post not available'} />;
+//     }
+
+//     return <PostPage post={post} />;
+// }
 
 import PostPage from '@/components/sections/PostPage';
 import NotFound from '@/components/shared/NotFound';
@@ -9,19 +31,18 @@ interface Params {
 }
 
 export default async function SinglePostPage({ params }: { params: Params }) {
-    // try {
     console.log("Fetching post with slug:", params.slug);
+
+    // Fetch the post from the database
     const post = await prisma.post.findUnique({
         where: { slug: params.slug },
     });
 
+    // Return NotFound component if post is not found
     if (!post) {
-        return <NotFound message={'Post not available'} />; // Return a fallback message
+        return <NotFound message={'Post not available'} />;
     }
 
+    // Return the PostPage component with the fetched post data
     return <PostPage post={post} />;
-    // } catch (error) {
-    //     console.error("Error fetching post:", error);
-    //     return <p>Error: Failed to fetch post</p>; // Gracefully handle the error
-    // }
 }
